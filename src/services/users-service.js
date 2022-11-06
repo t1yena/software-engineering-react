544f106050bafb37c45f7d3a3d30e87432a0c9f3
Yena <t1yena@gmail.com>
2022-11-06 16:54:25 -0500

    first commit

--- src/services/users-service.js
+++ src/services/users-service.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://my-node-express-project-env.eba-hxq4pgvm.us-east-1.elasticbeanstalk.com";
+const BASE_URL = "http://a2-env.eba-vyhiu8vb.us-east-1.elasticbeanstalk.com";
 // const BASE_URL = "https://software-engineering-node-fa22.herokuapp.com/api";
 // const BASE_URL = "http://localhost:4000/api";
 
@@ -23,7 +23,7 @@ export const deleteUser = (uid) =>
     .then(response => response.data);
 
 export const deleteUsersByUsername = (username) =>
-  axios.get(`${USERS_API}/username/${username}/delete`)
+  axios.delete(`${USERS_API}/username/${username}/delete`)
     .then(response => response.data);
 
 export const findUserByCredentials = (credentials) =>
--- src/tests/tuits-service.test.js
+++ src/tests/tuits-service.test.js
@@ -1,9 +1,74 @@
+import {
+  createTuit, deleteTuit, findAllTuits, findTuitById
+} from "../services/tuits-service"
+import { createUser, deleteUsersByUsername } from "../services/users-service";
+
 describe('can create tuit with REST API', () => {
-  // TODO: implement this
+  const ripley = {
+    username: 'ellenripley',
+    password: 'lv426',
+    email: 'ellenripley@aliens.com'
+  };
+
+  const ripleyTuit = {
+    tuit: 'Testing creating tuit!',
+    postedBy: ripley.username
+  }
+
+  // setup test before running test
+  beforeAll(() => {
+    return createUser(ripley.username);
+  })
+
+  // clean up after test runs
+  afterAll(() => {
+    // remove any data we created
+    return deleteUsersByUsername(ripley.username);
+  })
+
+  test('can create tuit with REST API', async () => {
+    // insert new tuit in the database
+    const newTuit = await createTuit(ripleyTuit);
+
+    // verify inserted tuit's properties match parameter tuit
+    expect(newTuit.tuit).toEqual(ripleyTuit.tuit);
+  });
+
 });
 
 describe('can delete tuit wtih REST API', () => {
-  // TODO: implement this
+  const ripley = {
+    username: 'ellenripley',
+    password: 'lv426',
+    email: 'ellenripley@aliens.com'
+  };
+
+  const testTuit = {
+    tuit: 'Testing deleting tuit!',
+    postedBy: ripley.username
+  };
+  let newTuit;
+  // setup the tests before verification
+  beforeAll(async() => {
+    newTuit = await createTuit(testTuit);
+    return newTuit;
+  });
+
+  // clean up after test runs
+  // afterAll(() => {
+  //   // remove any data we created
+  //   return deleteTuit(newTuit._id);
+  // });
+
+  test('can delete tuits from REST API', async () => {
+    // delete a user by their username. Assumes user already exists
+    const status = await deleteTuit(newTuit._id);
+
+    // verify we deleted at least one user by their username
+    expect(status.deletedCount).toBeGreaterThanOrEqual(1);
+
+
+});
 });
 
 describe('can retrieve a tuit by their primary key with REST API', () => {
