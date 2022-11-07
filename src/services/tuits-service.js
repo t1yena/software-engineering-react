6e4d5ee923efb3523db2ec32f1ac5879b14b1b55
Yena <t1yena@gmail.com>
2022-11-07 17:03:39 -0500

    finished a3

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const BASE_URL = "http://a2-env.eba-vyhiu8vb.us-east-1.elasticbeanstalk.com"
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = "http://a2-env.eba-vyhiu8vb.us-east-1.elasticbeanstalk.com"
+// const BASE_URL = "http://localhost:4000";
 const TUITS_API = `${BASE_URL}/api/tuits`;
 const USERS_API = `${BASE_URL}/api/users`;
 
--- src/tests/services.js
+++ src/tests/services.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const BASE_URL = "https://cs5500-01-sp22.herokuapp.com/api";
-const BASE_URL = "http://localhost:4000/api";
+const BASE_URL = "http://a2-env.eba-vyhiu8vb.us-east-1.elasticbeanstalk.com/api";
+// const BASE_URL = "http://localhost:4000/api";
 
 const LOGIN_API = `${BASE_URL}/login`;
 const USERS_API = `${BASE_URL}/users`;
