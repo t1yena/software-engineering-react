a2815a7460951cdc2c8236cc1e6781e97db7ec72
Yena <t1yena@gmail.com>
2022-11-20 12:25:16 -0500

    changed base url

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-const BASE_URL = process.env.REACT_APP_BASE_URL;
-
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = "http://a2-env.eba-vyhiu8vb.us-east-1.elasticbeanstalk.com";
 const AUTH_API = `${BASE_URL}/api/auth`;
 axios.defaults.adapter = require('axios/lib/adapters/http');
 
--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-// const BASE_URL = "http://a2-env.eba-vyhiu8vb.us-east-1.elasticbeanstalk.com"
+const BASE_URL = "http://a2-env.eba-vyhiu8vb.us-east-1.elasticbeanstalk.com"
 // const BASE_URL = "http://localhost:4000";
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+// const BASE_URL = process.env.REACT_APP_BASE_URL;
 const TUITS_API = `${BASE_URL}/api/tuits`;
 const USERS_API = `${BASE_URL}/api/users`;
 
