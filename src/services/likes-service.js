acdd8e250f8cc57f12e0e7cf48dc687b90d09709
Yena <t1yena@gmail.com>
2022-11-20 12:41:51 -0500

    local server

--- src/services/likes-service.js
+++ src/services/likes-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-const BASE_URL = "http://a2-env.eba-vyhiu8vb.us-east-1.elasticbeanstalk.com";
+// const BASE_URL = "http://a2-env.eba-vyhiu8vb.us-east-1.elasticbeanstalk.com";
 
-// const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 const USERS_API = `${BASE_URL}/api/users`;
 
 const api = axios.create({
